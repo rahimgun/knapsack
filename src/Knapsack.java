72bc05f4501b89ffe03c749acdb3c1645e3155f7
rahim gün <rahimgun2@gmail.com>
2019-05-20 22:02:14 +0300

    memoizing utility added

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -12,26 +12,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Knapsack {
-	public static void knapsack(int knapsack[][], int items[][]) {
+	public static void knapsack(int knapsack[][], int items[][],int memo[][]) {
 		int row = knapsack.length;
 		int column = knapsack[0].length;
 		int knapsackCapacity = column - 1;
 		for(int i = 0 ; i < row ; i++) {
 			for(int j = 0; j < column ;j ++) {
-				knapsack[i][j] = F(items , knapsack , i, j);
+				knapsack[i][j] = F(items , knapsack , i, j,memo);
 			}
 		}
 	}
-	public static int F(int[][] items, int[][] knapsack, int i, int knapsackCapacity) {
-		if(i == 0 || knapsackCapacity == 0) {
-			return 0;
+	public static int F(int[][] items, int[][] knapsack, int i, int knapsackCapacity,int memo[][]) {
+		int result;
+		if(memo[i][knapsackCapacity] != -1){
+			return memo[i][knapsackCapacity];
 		}
-		if(knapsackCapacity - items[i][1] < 0) {
-			return F(items, knapsack, i - 1, knapsackCapacity);
+		if(i == 0 || knapsackCapacity == 0) {
+			result = 0;
+		}
+		else if(knapsackCapacity - items[i][1] < 0) {
+			result =  F(items, knapsack, i - 1, knapsackCapacity,memo);
 		}
 		else {
-			return Math.max(F(items, knapsack, i - 1, knapsackCapacity), items[i][0] + F(items, knapsack, i - 1, knapsackCapacity - items[i][1]));
+			result =  Math.max(F(items, knapsack, i - 1, knapsackCapacity,memo), items[i][0] + F(items, knapsack, i - 1, knapsackCapacity - items[i][1],memo));
 		}
+		memo[i][knapsackCapacity] = result;
+		return result;
 	}
 	public static int[] findSubset(int[][] knapsack , int[][] items) {
 		int numberOfitems = knapsack.length - 1;
@@ -67,14 +73,20 @@ public class Knapsack {
 		}
 		scan.close();
 		int knapsack[][] = new int[numberOfItems + 1][knapsackCapacity + 1];
+		int memo[][] = new int[numberOfItems + 1][knapsackCapacity + 1];
+		for(int i2 = 0;i2<memo.length;i2++ ){
+			for(int i3 = 0;i3<memo[0].length;i3++){
+				memo[i2][i3] = -1;
+			}
+		}
 		/*for(int a = 0; a < numberOfItems + 1 ; a++) {
 			knapsack[a][0] = 0;
 		}
 		for(int a = 0; a <knapsackCapacity + 1 ; a++) {
 			knapsack[0][a] = 0;
 		}*/
-		knapsack(knapsack, items);
-		System.out.println(knapsack[4][20]);
+		knapsack(knapsack, items,memo);
+		System.out.println(knapsack[numberOfItems][knapsackCapacity]);
 		int[] subset = new int[numberOfItems + 1];
 		subset = findSubset(knapsack, items);
 		for(int i1 = 1;i1<subset.length;i1++) {
